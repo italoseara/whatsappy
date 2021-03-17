477e8153f3bcda1595ae0d7c9a7aacb8bfb53319
Italo Seara <54297273+italoseara@users.noreply.github.com>
2021-03-17 09:01:14 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(
     name='whatsappy',
     version='0.0.1',
@@ -13,4 +16,16 @@ setup(
         "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
         "Operating System :: OS Independent",
     ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires = [
+        "selenium ~= 3.141.0",
+        "Send2Trash ~= 1.5.0",
+        "webdriver-manager ~= 3.2.2",
+    ],
+    extra_requires = {
+        "dev": [
+            "pytest>=3.7",
+        ]
+    }
 )
