2545d3cfe5771f3a14207589b7f33cc1ee9a2fe1
Italo Seara <italo.sseara@gmail.com>
2022-01-31 11:06:50 -0300

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="whatsappy-py",
-    version="3.3.1",
+    version="3.3.2",
     description="Whatsappy is a Python library for creating whatsapp bots.",
     packages=["whatsappy"],
     classifiers=[
